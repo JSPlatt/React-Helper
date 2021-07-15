dd7f520ff4d269f1c4e4e675141723aaf61ca200
Jeffrey Platt <jsplatt@gmail.com>
2021-07-15 15:41:37 -0400

    calls to api/protected api working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react'
+import axios from 'axios';
 
 function App() {
 
@@ -11,6 +12,20 @@ function App() {
     isAuthenticated,
     } = useAuth0()
 
+    function callApi() {
+      axios
+        .get("http://localhost:4000")
+        .then(res => console.log(res.data))
+        .catch(error => console.log (error.message))
+    }
+
+    function callProtectedApi() {
+      axios
+        .get("http://localhost:4000/protected")
+        .then(res => console.log(res.data))
+        .catch(error => console.log (error.message))
+    }
+
   return (
     <div className="App">
       <h1>Auth0 Authentication</h1>
@@ -20,6 +35,14 @@ function App() {
           <li><button onClick={logout}>Logout</button></li>
         </ul>
         <h3>User is { isAuthenticated? "Logged in" : "Not logged in" }</h3>
+          <ul>
+              <li>
+                  <button onClick={callApi}>Call API route</button>
+              </li>
+              <li>
+                  <button onClick={callProtectedApi}>Call Protected API route</button>
+              </li>
+          </ul>
         { isAuthenticated && (
         <pre style={{textAlign: 'start'}}>{JSON.stringify(user, null, 2)}
         </pre>
